d006864acbdd827c38a347618e7dde9ab7e450f5
Abegael Jackson <abegaeljackson@abegaels-mbp.hitronhub.home>
2017-01-12 18:29:50 -0800

    Initial Commit

---
+++ DeveloperWorkLog/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DeveloperWorkLog
+//
+//  Created by Abegael Jackson on 2017-01-12.
+//  Copyright © 2017 Abegael Jackson. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ DeveloperWorkLog/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DeveloperWorkLog
+//
+//  Created by Abegael Jackson on 2017-01-12.
+//  Copyright © 2017 Abegael Jackson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
