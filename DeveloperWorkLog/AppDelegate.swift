f886aec4eedbf00cd666a2b409cf2c340aa87ded
Abbey Jackson <abbey@abbeyjackson.ca>
2017-01-13 23:11:26 -0800

    Menu bar status item displaying, popover vc displaying with simple text fields for entry and tableview below. Basic app set up and MacOS exploring done.

--- DeveloperWorkLog/AppDelegate.swift
+++ DeveloperWorkLog/AppDelegate.swift
@@ -10,17 +10,48 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
+    var popover = NSPopover()
+    var statusItem: NSStatusItem?
+    var darkModeOn: Bool = false
 
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        
+        statusItem = NSStatusBar.system().statusItem(withLength: NSVariableStatusItemLength)
+        statusItem?.toolTip = "Developer Work Log"
+        
+        if let button = statusItem?.button {
+            button.action = #selector(togglePopover)
+            button.sendAction(on: [.leftMouseUp, .rightMouseUp])
+            button.image = NSImage(named: "book-icon")
+        }
+        
+        popover.behavior = .transient
+        
+        let storyboard = NSStoryboard.init(name: "Main", bundle: Bundle.main)
+        if let workLogVC = storyboard.instantiateController(withIdentifier: "workLogVC") as? WorkLogVC {
+            popover.contentViewController = workLogVC
+        }
+        
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    // MARK: Helpers
+    
+    @objc func togglePopover() {
+        
+        if popover.isShown {
+            popover.close()
+        }
+        else {
+            if let button = statusItem?.button {
+                popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
+            }
+        }
+    }
 }
 
